8997833665d26eaa45e591ddbbb696e5bc03c395
Olga Morozova <Morozova_Olga_22@mail.ru>
2021-11-03 02:38:14 +0300

    Added makeDictionary method

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -1,13 +1,20 @@
 package ru.netology;
 
+import java.util.List;
 import java.util.Scanner;
+import java.util.function.Function;
 
 public class Main {
     public static void main(String[] args) {
         try (final Scanner scanner = new Scanner(System.in)) {
             System.out.println("Введите строку");
             final String inputLine = scanner.nextLine();
-            new DictionaryMaker().apply(inputLine).forEach(System.out::println);
+            makeDictionary(inputLine, new DictionaryMaker());
         }
+    }
+
+    private static void makeDictionary(String inputLine, Function<String, List<String>> function) {
+        function.apply(inputLine)
+                .forEach(System.out::println);
     }
 }
